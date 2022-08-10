3156b1d915ba76cd7a2a406b4ae080c17135c4b2
Hannah Alverson <alverson98@yahoo.com>
2022-08-10 11:44:07 -0400

    added variable declarations for html elements

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// VARIABLE DECLARATIONS
+
+// Header Variables
+var header = document.getElementById("header");
+var highScoreButton = document.getElementById("high-score-button");
+var timer = document.getElementById("timer");
+
+// Starting Page Variables
+var startingPage = document.getElementById("starting-page");
+var startButton = document.getElementById("start-button");
+
+// Question Page Variables
+var questionPage = document.getElementById("question-page");
+
+// Initial Page Variables
+var initialPage = document.getElementById("initial-page");
+var initialInput = document.getElementById("initial");
+var submitButton= document.getElementById("submit-button");
+
+// High Score Page
+var highScorePage = document.getElementById("high-score-page");
+var highScores = document.getElementById("high-scores");
+var backButton = document.getElementById("back-button");
+var clearScoreButton = document.getElementById("clear-score-button");
