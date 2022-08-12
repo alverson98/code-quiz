88b7090f4dcd922a631e3150c66f67e5f6836ea3
Hannah Alverson <alverson98@yahoo.com>
2022-08-11 21:57:43 -0400

    created functions to display all the questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // VARIABLE DECLARATIONS
-
 // Header Variables
 var header = document.getElementById("header");
 var highScoreButton = document.getElementById("high-score-button");
@@ -15,6 +14,10 @@ var showQuestion = document.getElementById("show-question");
 var showAnswers = document.getElementById("show-answers");
 var answerStatus = document.getElementById("answer-status");
 
+// Correct vs Incorrect Statement variables
+var correct = "Correct!";
+var incorrect = "Incorrect!";
+
 // Initial Page Variables
 var initialPage = document.getElementById("initial-page");
 var initialInput = document.getElementById("initial");
@@ -28,76 +31,171 @@ var clearScoreButton = document.getElementById("clear-score-button");
 
 // Questions & Answers
 var q1 = {
-  question: "1. Commonly used data types DO NOT include...",
+  question: "Commonly used data types DO NOT include...",
   answers: ["Strings", "Booleans", "Alerts", "Numbers"],
-  correctIndex: 2,
+  correctA: "Alerts",
 };
 var q2 = {
-  question: "2. The condition in an if / else statement is enclosed with ____.",
+  question: "The condition in an if / else statement is enclosed with ____.",
   answers: ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
-  correctIndex: 2,
+  correctA: "Parenthesis",
 };
-var q2 = {
-  question: "3. Arrays in JavaScript can be used to store ____.",
+var q3 = {
+  question: "Arrays in JavaScript can be used to store ____.",
   answers: [
     "Numbers & Strings",
     "Other arrays",
     "Booleans",
     "All of the above",
   ],
-  correctIndex: 3,
-};
-var q3 = {
-  question:
-    "4. String values must be enclosed within _____ when being assigned to variables.",
-  answers: ["Commas", "Curly brackets", "Quotes", "Parenthesis"],
-  correctIndex: 2,
+  correctA: "All of the above",
 };
 var q4 = {
   question:
-    "5. A very useful tool used during development and debugging for printing content to the debugger is...",
-  answers: ["JavaScript", "Terminal/ Bash", "For loops", "Console.log"],
-  correctIndex: 3,
+    "String values must be enclosed within _____ when being assigned to variables.",
+  answers: ["Commas", "Curly brackets", "Quotes", "Parenthesis"],
+  correctA: "Quotes",
 };
 var q5 = {
+  question:
+    "A very useful tool used during development and debugging for printing content to the debugger is...",
+  answers: ["JavaScript", "Terminal/ Bash", "For loops", "Console.log"],
+  correctA: "Console.log",
+};
+var q6 = {
   question: "6. Coding is awesome. T/F",
   answers: ["True", "False"],
-  correctIndex: 0,
+  correctA: "True",
 };
 
 // Array with all of the question variables
-var questions = [q1, q2, q3, q4, q5];
+var questions = [q1, q2, q3, q4, q5, q6];
 console.log(questions);
 
 // Setting timer - 15 seconds/question
 var quizTime = questions.length * 15;
 console.log(quizTime);
 
-// Starting Page
+// EVENT LISTENERS
+//Start button- starts game
 startButton.addEventListener("click", function (event) {
   event.preventDefault;
+  startTimer();
   beginQuiz();
 });
 
-// Begin the quiz
-function beginQuiz() {
-  // Timer for quiz
+// Starting timer
+function startTimer() {
   var quizTimer = setInterval(function () {
     quizTime--;
     timer.textContent = quizTime;
-    displayQuestions();
 
     if (quizTime === 0) {
       clearInterval(quizTimer);
       // Go to initial page once timer done
     }
   }, 1000);
+}
+
+// Begin the quiz
+function beginQuiz() {
+  displayQuestions();
 
   // Questions
   function displayQuestions() {
-    for (var i = 0; i < questions.length; i++) {
-      if (i === 1) {
+    // Display 1st question
+    showQ1();
+    function showQ1() {
+      showQuestion.textContent = questions[0].question;
+      for (let i = 0; i < q1.answers.length; i++) {
+        var answerList = document.createElement("li");
+        showAnswers.appendChild(answerList);
+        var answerButton = document.createElement("button");
+        console.log(answerButton);
+        answerList.appendChild(answerButton);
+        var answerOptions = q1.answers[i];
+        answerButton.textContent = answerOptions;
 
+        answerButton.addEventListener("click", function (event) {
+          event.preventDefault;
+          showQ2();
+        });
+      }
+    }
+    // Display 2nd question
+    function showQ2() {
+      showQuestion.textContent = questions[1].question;
+      for (let i = 0; i < q2.answers.length; i++) {
+        var answerlist = document.createElement("li");
+        showAnswers.appendChild(answerlist);
+        var answerButton = document.createElement("button");
+        answerlist.appendChild(answerButton);
+        var answerOptions = q2.answers[i];
+        answerButton.textContent = answerOptions;
+      }
+      answerButton.addEventListener("click", function (event) {
+        event.preventDefault;
+        showQ3();
+      });
+    }
+    // Display 3rd question
+    function showQ3() {
+      showQuestion.textContent = questions[2].question;
+      for (let i = 0; i < q3.answers.length; i++) {
+        var answerlist = document.createElement("li");
+        showAnswers.appendChild(answerlist);
+        var answerButton = document.createElement("button");
+        answerlist.appendChild(answerButton);
+        var answerOptions = q3.answers[i];
+        answerButton.textContent = answerOptions;
+      }
+      answerButton.addEventListener("click", function (event) {
+        event.preventDefault;
+        showQ4();
+      });
+    }
+    // Display 4th question
+    function showQ4() {
+      showQuestion.textContent = questions[3].question;
+      for (let i = 0; i < q4.answers.length; i++) {
+        var answerlist = document.createElement("li");
+        showAnswers.appendChild(answerlist);
+        var answerButton = document.createElement("button");
+        answerlist.appendChild(answerButton);
+        var answerOptions = q4.answers[i];
+        answerButton.textContent = answerOptions;
+      }
+      answerButton.addEventListener("click", function (event) {
+        event.preventDefault;
+        showQ5();
+      });
+    }
+    // Display 5th question
+    function showQ5() {
+      showQuestion.textContent = questions[4].question;
+      for (let i = 0; i < q5.answers.length; i++) {
+        var answerlist = document.createElement("li");
+        showAnswers.appendChild(answerlist);
+        var answerButton = document.createElement("button");
+        answerlist.appendChild(answerButton);
+        var answerOptions = q5.answers[i];
+        answerButton.textContent = answerOptions;
+      }
+      answerButton.addEventListener("click", function (event) {
+        event.preventDefault;
+        showQ6();
+      });
+    }
+    // Display 6th question
+    function showQ6() {
+      showQuestion.textContent = questions[5].question;
+      for (let i = 0; i < q6.answers.length; i++) {
+        var answerlist = document.createElement("li");
+        showAnswers.appendChild(answerlist);
+        var answerButton = document.createElement("button");
+        answerlist.appendChild(answerButton);
+        var answerOptions = q6.answers[i];
+        answerButton.textContent = answerOptions;
       }
     }
   }
